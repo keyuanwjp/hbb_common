e3ba7269a3aa601b8b85fa24d17dd7d60019dbdd
keyuanwjp <147332099+keyuanwjp@users.noreply.github.com>
2025-08-02 12:07:08 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["szgrsoft.com"];
+pub const RS_PUB_KEY: &str = "WVFqeKSQoNiZn3RosP7ylLebI0g+37nhRi3NlsanQUE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
